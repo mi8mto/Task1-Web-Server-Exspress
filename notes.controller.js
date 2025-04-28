b44742177170948e4360d5010242ec867a0ae318
Igor Manakhau <mi8mto@gmail.com>
2025-04-28 19:01:25 +0300

    Add all files

---
+++ notes.controller.js
@@ -0,0 +1,63 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+
+  await saveNotes(notes);
+  console.log(chalk.bgGreen("Note was added!"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.bgWhite(note.id), chalk.blue(note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const filtered = notes.filter((note) => note.id !== id);
+  await saveNotes(filtered);
+  console.log(chalk.red(`Note with id="${id}" has been removed.`));
+}
+
+async function editNote(id, newTitle) {
+  const notes = await getNotes();
+  const updated = notes.map((note) => {
+    if (note.id === id) {
+      return { ...note, title: newTitle };
+    }
+    return note;
+  });
+  await saveNotes(updated);
+  console.log(chalk.blue(`Note with id="${id}" has been updated.`));
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+  getNotes,
+  editNote,
+};
---
+++ public/app.js
@@ -0,0 +1,40 @@
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "remove") {
+    const id = event.target.dataset.id;
+    console.log("remove", id);
+
+    remove(id).then(() => {
+      event.target.closest("li").remove();
+    });
+  }
+
+  if (event.target.dataset.type === "edit") {
+    const id = event.target.dataset.id;
+    const currentTitle = event.target
+      .closest("li")
+      .firstChild.textContent.trim();
+    const newTitle = prompt("Введите новое название", currentTitle);
+
+    if (newTitle) {
+      edit(id, newTitle).then(() => {
+        event.target.closest("li").firstChild.textContent = newTitle;
+      });
+    }
+  }
+});
+
+async function remove(id) {
+  await fetch(`/${id}`, {
+    method: "DELETE",
+  });
+}
+
+async function edit(id, title) {
+  await fetch(`/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title }),
+  });
+}
